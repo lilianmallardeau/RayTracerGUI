109478fa69522e902df786a6dc453eb076b3fa15
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2021-03-17 20:27:01 +0100

    stash

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QStandardItemModel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -21,6 +22,20 @@ MainWindow::MainWindow(QWidget *parent)
     //ui->PicturePreview->setScaledContents(true);
 
     ui->PicturePreview->setPixmap(img);
+
+//    QStandardItemModel* model = new QStandardItemModel();
+
+//    QStandardItem* item0 = new QStandardItem(QIcon("test.png"), "1 first item");
+//    QStandardItem* item1 = new QStandardItem(QIcon("test.png"), "2 second item");
+//    QStandardItem* item3 = new QStandardItem(QIcon("test.png"), "3 third item");
+//    QStandardItem* item4 = new QStandardItem("4 forth item");
+
+//    model->appendRow(item0);
+//    item0->appendRow(item3);
+//    item0->appendRow(item4);
+//    model->appendRow(item1);
+
+//    ui->SceneList->setModel(model);
 }
 
 MainWindow::~MainWindow()
